ad15790affe6052bf121844f6af31f2ae2d41a49
anuj <kumaranuj1303@gmail.com>
2025-03-19 10:45:04 +0530

    fix: add jotai for state management

--- app/components/Welcome.tsx
+++ app/components/Welcome.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import { stateManagementAtom } from "app/states/atoms";
+import { useAtomValue } from "jotai";
 
 export function Welcome() {
-  const [count, setCount] = useState(0);
+  const stateManagement = useAtomValue(stateManagementAtom);
   return (
     <>
       <main>
         <h2>Starting Point</h2>
+        <p>State Management: {stateManagement.toUpperCase()}</p>
       </main>
     </>
   );
---
+++ app/states/atoms.ts
@@ -0,0 +1,3 @@
+import { atom } from "jotai";
+
+export const stateManagementAtom = atom("jotai");
