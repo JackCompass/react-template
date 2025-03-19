85cd9a6d2741dcc383aeae2a03f781a02769ccca
anuj <kumaranuj1303@gmail.com>
2025-03-19 15:21:33 +0530

    fix: api loaders directly present in the react router

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,6 +5,24 @@ export function meta({}: Route.MetaArgs) {
   return [{ title: "Home" }, { name: "description", content: "Welcome to the home page" }];
 }
 
-export default function Home() {
+export default function Home(props: Route.ComponentProps) {
+  const { loaderData, actionData, params, matches } = props;
+  console.log("loaderData", loaderData);
+  /**
+   * loaderData: Only called on the server when server rendering or at the build time with pre-rendering.
+   * actionData:
+   */
   return <Welcome />;
+}
+
+export async function loader() {
+  return {
+    message: "Text from the server loader",
+  };
+}
+
+export async function clientLoader() {
+  return {
+    message: "Text from the client loader",
+  };
 }
